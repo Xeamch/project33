12f84d2f8f29869541d13e02dc38b50908510ded
Sergey Barmin <Xeamch@yandex.ru>
2020-03-06 02:23:48 +0300

    pravkajs

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/arrow/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/arrow/chevron-right-solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="/img/slider/arrow/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/img/slider/arrow/chevron-right-solid.png"></button>',
         responsive: [
             {
                 breakpoint: 1024,
