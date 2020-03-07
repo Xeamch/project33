eb0bf099a4c858c4548364a291b0adffa455aaa0
Sergey Barmin <Xeamch@yandex.ru>
2020-03-08 00:31:52 +0300

    server

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,46 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__desc').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Пожалуйста введите свое имя",
+        phone: "Пожалуйста введите свой номер",
+        email: {
+          required: "Пожалуйста введите свою почту",
+          email: "Ваш e-mail адрес должен быть в формате name@domain.com"
+        }
+      }
+    });
+  };
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99")
 });
