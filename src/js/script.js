6f1efa5ec44700ebead2d8c941554650b4752dd2
Sergey Barmin <Xeamch@yandex.ru>
2020-03-10 00:03:08 +0300

    projectcomplite

--- src/js/script.js
+++ src/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
           $(this).on('click', function(e) {
               e.preventDefault();
               $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+              $('.catalog-item__listhr').eq(i).toggleClass('catalog-item__listhr_active');
           })
       });
   };
@@ -75,5 +75,42 @@ $(document).ready(function(){
   validateForms('#consultation form');
   validateForms('#order form');
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99")
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //sroll
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+  });
+
+  $("a[href^='#up']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
+
+
 });
